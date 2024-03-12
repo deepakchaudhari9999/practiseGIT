5c8f64996de85ccde80dff002a64ae8cde7d50f1
deepakchaudhari9999 <deepakchaudhari9999@gmail.com>
2024-03-12 13:22:04 +0530

    added search_customer file and modified edit_customer file

--- src/test/java/TestNG_Parameter/testNG_1/edit_customer.java
+++ src/test/java/TestNG_Parameter/testNG_1/edit_customer.java
@@ -1,2 +1,10 @@
 // This is new Test case
 //This is for testing purpose.
+
+
+
+
+
+//This is code added newly. for git github testing.
+//123456789
+//Hello
---
+++ src/test/java/TestNG_Parameter/testNG_1/search_customer.java
@@ -0,0 +1,7 @@
+package TestNG_Parameter.testNG_1;
+
+public class search_customer {
+	
+	// This is for git github testing
+
+}
